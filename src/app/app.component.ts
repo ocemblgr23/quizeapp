b801c5b47921003caa828ee8e64166ce6db080e1
ocemblgr23 <131694566+ocemblgr23@users.noreply.github.com>
2023-08-24 20:35:15 +0530

    read data from local json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import {AfterViewChecked, Component, OnChanges, OnInit} from '@angular/core';
 import {CartService} from "./shared/cart.service";
 import {Observable} from "rxjs";
 import {IProduct} from "./shared/product.model";
-
+import * as products from "../assets/products.json";
+import {ProductsService} from "./shared/products.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,13 +13,16 @@ export class AppComponent implements OnInit,AfterViewChecked,OnChanges{
   title = 'quizeapp';
   $sharedMessage: Observable<string>;
   cartItems:IProduct[]=[];
-  constructor(private cartService:CartService){
+  constructor(private cartService:CartService,private products:ProductsService){
   this.$sharedMessage = this.cartService.getSharedData();
 }
   ngOnInit() {
     console.log('called')
     this.cartService.getCartItems().subscribe(p=>{
      this.cartItems = p;
+    })
+    this.products.getProductsFromLoc().subscribe(p=>{
+      console.log(p)
     })
   }
   ngAfterViewChecked() {
